751e54b85751be49e0665f1dd94dcf5d9bf1bd2f
nakajimamoto340 <nakajimamoto340@gmail.com>
2023-10-15 10:09:46 +0900

    ログイン・ログアウトの実装

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,9 @@
 # from django.shortcuts import render
+from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
-
-from mysite.settings import LOGIN_REDIRECT_URL
 
 from .forms import SignupForm
 
@@ -12,7 +11,7 @@ from .forms import SignupForm
 class SignupView(CreateView):
     form_class = SignupForm
     template_name = "accounts/signup.html"
-    success_url = reverse_lazy(LOGIN_REDIRECT_URL)
+    success_url = reverse_lazy(settings.LOGIN_REDIRECT_URL)
 
     def form_valid(self, form):
         response = super().form_valid(form)
