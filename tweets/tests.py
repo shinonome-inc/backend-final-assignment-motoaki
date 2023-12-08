51942c8ca10bcff5ca847cbbf6b0e6f12d4d6d9f
nakajimamoto340 <nakajimamoto340@gmail.com>
2023-12-08 22:21:04 +0900

    テストの実装

--- tweets/tests.py
+++ tweets/tests.py
@@ -12,12 +12,17 @@ class TestHomeView(TestCase):
         self.user = User.objects.create_user(username="tester", password="password")
         self.client.force_login(self.user)
         self.tweet = Tweet.objects.create(content="tester", user=self.user)
+        Tweet.objects.create(content="Test Tweet 1", user=self.user)
+        Tweet.objects.create(content="Test Tweet 2", user=self.user)
 
     def test_success_get(self):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, 200)
+        tweets_in_context = response.context["tweets"]
+        tweets_in_db = Tweet.objects.all()
+        self.assertEqual(len(tweets_in_context), len(tweets_in_db))
         self.assertTemplateUsed(response, "tweets/home.html")
-        self.assertQuerysetEqual(response.context["tweets"], Tweet.objects.all())
+        self.assertQuerysetEqual(response.context["tweets"], tweets_in_db)
 
 
 class TestTweetCreateView(TestCase):
@@ -33,9 +38,14 @@ class TestTweetCreateView(TestCase):
 
     def test_success_post(self):
         self.client.force_login(self.user)
+        initial_tweet_count = Tweet.objects.count()
         response = self.client.post("/tweets/create/", {"content": "A test tweet"})
         self.assertEqual(response.status_code, 302)
+        self.assertEqual(Tweet.objects.count(), initial_tweet_count + 1)
+        self.assertRedirects(response, reverse("tweets:home"))
         self.assertTrue(Tweet.objects.filter(user=self.user, content="A test tweet").exists())
+        new_tweet = Tweet.objects.latest("created_at")
+        self.assertEqual(new_tweet.content, "A test tweet")
 
     def test_failure_post_with_empty_content(self):
         invalid_data = {"content": ""}
@@ -52,6 +62,8 @@ class TestTweetCreateView(TestCase):
         response = self.client.post("/tweets/create/", {"content": "A" * 281})
         self.assertEqual(response.status_code, 200)
         self.assertFalse(Tweet.objects.exists())
+        form = response.context["form"]
+        self.assertIn("この値は 280 文字以下でなければなりません( 281 文字になっています)。", form.errors["content"])
 
 
 class TestTweetDetailView(TestCase):
@@ -64,6 +76,8 @@ class TestTweetDetailView(TestCase):
         response = self.client.get(f"/tweets/{self.tweet.id}/")
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, "tweets/tweet_detail.html")
+        tweets_in_context = response.context["tweet"]
+        self.assertEqual(tweets_in_context, self.tweet)
 
 
 class TestTweetDeleteView(TestCase):
@@ -78,16 +92,23 @@ class TestTweetDeleteView(TestCase):
         response = self.client.post(f"/tweets/{self.tweet.id}/delete/")
         self.assertEqual(response.status_code, 302)
         self.assertFalse(Tweet.objects.filter(pk=self.tweet.id).exists())
+        self.assertEqual(response.url, reverse("tweets:home"))
 
     def test_failure_post_with_not_exist_tweet(self):
         self.client.force_login(self.user)
+        self.tweet.delete()
         response = self.client.post("/tweets/9999/delete/")
         self.assertEqual(response.status_code, 404)
+        self.assertNotEqual(response.status_code, 200)
+        self.assertFalse(Tweet.objects.filter(pk=self.tweet.id).exists())
 
     def test_failure_post_with_incorrect_user(self):
         self.client.force_login(self.other_user)
         response = self.client.post(f"/tweets/{self.tweet.id}/delete/")
         self.assertEqual(response.status_code, 403)
+        self.assertNotEqual(response.status_code, 200)
+        self.assertTrue(Tweet.objects.filter(pk=self.tweet.id).exists())
+        self.assertEqual(Tweet.objects.get(pk=self.tweet.id).user, self.user)
 
 
 # class TestLikeView(TestCase):
