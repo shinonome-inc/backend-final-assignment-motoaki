0a5789d8be11437109855c03b01d0d61ec33c612
nakajimamoto340 <nakajimamoto340@gmail.com>
2023-12-15 01:28:56 +0900

    tweet機能の実装

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField()
+
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=280)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Tweet#{self.pk}: {self.content}"
